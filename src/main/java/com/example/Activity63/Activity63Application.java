3ee3474fe2cd6563cae0c98b8201d2890b440750
darinjbrown <darin.brown.cs@gmail.com>
2025-04-27 16:38:26 -0500

    initial commit

---
+++ src/main/java/com/example/Activity63/Activity63Application.java
@@ -0,0 +1,13 @@
+package com.example.Activity63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity63Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activity63Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Activity63/Activity63ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Activity63;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity63ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
